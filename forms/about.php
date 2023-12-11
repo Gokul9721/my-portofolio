d6267bfb0bd126606dd787ee1e16b69f08d42a67
Gokul9721 <gakul8137@gmail.com>
2023-12-11 22:24:03 +0545

    adding about page on gokul branch

---
+++ forms/about.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Us</title>
+    <!-- Add any necessary CSS styles or other meta tags -->
+</head>
+<body>
+    <header>
+        <h1>About Us</h1>
+        <!-- Add navigation or other header content if needed -->
+    </header>
+
+    <main>
+        <section>
+            <h2>Our Company</h2>
+            <p>Welcome to our website! We are dedicated to...</p>
+            <!-- Add more information about your company -->
+        </section>
+
+        <section>
+            <h2>Our Team</h2>
+            <ul>
+                <li>John Doe - CEO</li>
+                <li>Jane Smith - CTO</li>
+                <!-- Add more team members -->
+            </ul>
+            <!-- Add information about your team members -->
+        </section>
+
+        <section>
+            <h2>Contact Us</h2>
+            <p>You can reach us at:</p>
+            <p>Email: contact@company.com</p>
+            <p>Phone: 123-456-7890</p>
+            <!-- Add more contact information -->
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Company Name. All rights reserved.</p>
+        <!-- Add footer content such as copyright information -->
+    </footer>
+</body>
+</html>
